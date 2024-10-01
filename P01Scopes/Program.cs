f21ca6e9b9bdf8e96d5d3c3cdb815fe6f6ab13b9
JackBillingham <billingham.jacks@gmail.com>
2024-10-01 13:34:56 +0200

    103 start

---
+++ P01Scopes/Program.cs
@@ -0,0 +1,23 @@
+﻿{
+Console.WriteLine("Give me a number.");
+{
+    string userInput = Console.ReadLine();
+    
+    Console.WriteLine(userInput);
+}
+Console.WriteLine("Give me another number.");
+{
+    string userInput = Console.ReadLine();
+    int num = int.Parse(userInput);
+    Console.WriteLine(num);
+}
+{
+    Console.WriteLine("Give me another number.");
+    {
+        string userInput = Console.ReadLine();
+        char.Parse(userInput);
+        Console.WriteLine(userInput);
+    }
+        
+}
+}
