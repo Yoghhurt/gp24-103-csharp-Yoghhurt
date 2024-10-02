93b99e7b828e57ab347ae0cd1cac1c34857ccbd6
JackBillingham <billingham.jacks@gmail.com>
2024-10-02 13:57:57 +0200

    p01

--- P01Scopes/Program.cs
+++ P01Scopes/Program.cs
@@ -1,23 +1,32 @@
 ﻿{
-Console.WriteLine("Give me a number.");
-{
-    string userInput = Console.ReadLine();
-    
-    Console.WriteLine(userInput);
-}
-Console.WriteLine("Give me another number.");
-{
-    string userInput = Console.ReadLine();
-    int num = int.Parse(userInput);
-    Console.WriteLine(num);
-}
-{
-    Console.WriteLine("Give me another number.");
     {
-        string userInput = Console.ReadLine();
-        char.Parse(userInput);
-        Console.WriteLine(userInput);
-    }
+       
+        Console.WriteLine("Give me a number.");
+        string inputString = Console.ReadLine();
         
-}
+        if (int.TryParse(inputString, out _))
+        {
+            Console.WriteLine($"{inputString}1"); 
+        }
+        
+        Console.WriteLine("Give me another number.");
+        string inputIntString = Console.ReadLine();
+        
+        if (int.TryParse(inputIntString, out int inputInt))
+        {
+            inputInt++; 
+            Console.WriteLine(inputInt); 
+        }
+        
+        
+        Console.WriteLine("Give me another number.");
+        string inputCharString = Console.ReadLine();
+        
+        if (inputCharString.Length == 1 && char.TryParse(inputCharString, out char inputChar))
+        {
+            inputChar++; 
+            Console.WriteLine(inputChar); 
+        }
+      
+    }
 }
