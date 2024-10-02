dad0ab371d35795906a77783a3992cfc0243b1f0
JackBillingham <billingham.jacks@gmail.com>
2024-10-02 14:24:16 +0200

    P02

--- P02Functions/Program.cs
+++ P02Functions/Program.cs
@@ -30,4 +30,75 @@ static void IAmTheHelp()
     }
 }
 //P02_3
+{
+    
+    static void GenerateRandomNumber()
+    {
+        Random random = new Random();
+        int randomNumber = random.Next(1, 101); 
+        Console.WriteLine(randomNumber); 
+    }
+
+    static void Main()
+    {
+        
+        Console.WriteLine("Generating random numbers between 1 and 100:");
+        for (int i = 0; i < 5; i++)
+        {
+            GenerateRandomNumber();
+        }
+    }
+}
+
+//P02 Final
+
+{
+    
+    static void MoveForward(int steps)
+    {
+        for (int i = 0; i < steps; i++)
+        {
+            Console.WriteLine("Move Forward.");
+        }
+    }
+
+    
+    static void TurnRight()
+    {
+        Console.WriteLine("Turn Right.");
+    }
+
+    
+    static void TurnLeft()
+    {
+        Console.WriteLine("Turn Right."); 
+        TurnRight();
+        TurnRight();
+    }
+
+    static void Main()
+    {
+        
+        MoveForward(3);
+        
+        
+        TurnRight();
+        TurnRight();
+        MoveForward(3);
+        
+        
+        TurnLeft();
+        MoveForward(3);
+        
+        
+        TurnLeft();
+        MoveForward(1);
+        
+        
+        TurnRight();
+        TurnRight();
+        MoveForward(1);
+    }
+}
+
 
