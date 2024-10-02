edef77c4206faca9bd9ec00cf3256613545a092f
JackBillingham <billingham.jacks@gmail.com>
2024-10-02 14:16:56 +0200

    P02_2

---
+++ P02Functions/Program.cs
@@ -0,0 +1,33 @@
+﻿//P02_1
+
+{
+    static void HelloWorld()
+
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+    static void Main()
+    {
+        HelloWorld();
+    }
+}
+
+//P02_2
+{
+    static void AskForHelp()
+    {
+        Console.WriteLine("Asking for help!");
+        IAmTheHelp();
+    }
+static void IAmTheHelp()
+    {
+        Console.WriteLine("I am the help!");
+    }
+    static void Main()
+    {
+        AskForHelp();
+    }
+}
+//P02_3
+
