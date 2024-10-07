adedbff66da963ec4e71bdfbea2aae54cd03d523
JackBillingham <billingham.jacks@gmail.com>
2024-10-07 13:18:53 +0200

    P051

---
+++ P05_1OneParameter/Program.cs
@@ -0,0 +1,27 @@
+﻿//P05_0
+static void Main()
+{
+    things();
+}
+
+static string things()
+{
+    string Name = "Marc";
+    string Course = "Game Programming";
+    string School = "Forsbergs";
+    
+    return Name + Course + School;
+}
+
+static void Main2()
+{
+    doubled(5);
+}
+
+static int doubled(int value)
+{
+    int dubValue = value * 2; 
+}
+
+//P05_1
+
