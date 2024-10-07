36c656ead7842f65a122935379e4ef3997550af7
JackBillingham <billingham.jacks@gmail.com>
2024-10-07 09:45:14 +0200

    P03

--- P03CallStack/Program.cs
+++ P03CallStack/Program.cs
@@ -1,4 +1,25 @@
-﻿//P03_2
+﻿//P03_1
+
+{
+    DrawTriangle(5); 
+}
+
+static void DrawTriangle(int rows)
+{
+    
+    if (rows <= 0)
+    {
+        return;
+    }
+
+    
+    Console.WriteLine(new string('#', rows));
+
+    
+    DrawTriangle(rows - 1);
+}
+
+//P03_2
 void F1(){
     Console.Write("A"); // A
 }
@@ -15,6 +36,15 @@ void F4(){
     Console.Write("-"); // -A
     F1();
 }
+
+F2();
+F3();
+F2();
+F4();
+F1();
+F1();
+F4();
+F3();
 
 
 
