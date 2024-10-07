0d1f27925e6827b572229a4125756f02f11e2c1e
JackBillingham <billingham.jacks@gmail.com>
2024-10-07 10:13:35 +0200

    P04_1_1

---
+++ P04_1Return/Program.cs
@@ -0,0 +1,15 @@
+﻿//P04_1_1
+
+    while (true)
+    {
+        Console.WriteLine("Would you like to buy a game?");
+        if (Console.ReadLine() == ("yes"))
+        {
+            Console.WriteLine("Thank you.");
+            return;
+        }
+    }
+
+//P04_1_2
+
+
