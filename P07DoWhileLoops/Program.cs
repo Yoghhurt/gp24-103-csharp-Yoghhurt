10d4ab4fb7fb3f28e52a3fd84119e8bc0e3d8578
JackBillingham <billingham.jacks@gmail.com>
2024-10-08 14:56:03 +0200

    fattar ingenting.

---
+++ P07DoWhileLoops/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 1;
+int b = 3;
+int c = 41;
+int d = 103;
+
+    int askUserBetween(int min, int max);
+    {
+        do
+        {
+            Console.WriteLine("Gimme a number between {a} & {b}.");
+            string userInput = Console.ReadLine();
+            int userNumber = int.Parse(userInput);
+            
+            if (userNumber >= a && userNumber <= b) ;
+            Console.WriteLine($"You gave me {userNumber}.");
+        } while ();
+    }
+
