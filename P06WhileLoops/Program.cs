ae196c5e8f877d99e98dcd332ac1fb355a93a92d
JackBillingham <billingham.jacks@gmail.com>
2024-10-08 13:10:19 +0200

    P06

---
+++ P06WhileLoops/Program.cs
@@ -0,0 +1,10 @@
+﻿int matches = 10;
+while (matches > 0)
+{
+    Console.WriteLine($"There are {matches} matches left.");
+    Console.WriteLine($"How many matches would you like to draw?");
+    string input = Console.ReadLine();
+    int userMatches = int.Parse(input);
+    matches -= userMatches;
+}
+Console.WriteLine("Game Over");
