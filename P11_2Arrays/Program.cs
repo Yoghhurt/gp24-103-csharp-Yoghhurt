54e8da01e9b2e8d8c2c5c1d2aebdbfd839e1478b
JackBillingham <billingham.jacks@gmail.com>
2024-10-12 15:13:12 +0200

    P11:2

---
+++ P11_2Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿void RandomCounts()
+{
+    int[] counts = new int[11];
+    
+    Random random = new Random();
+    
+    for (int i = 0; i < 10000; i++)
+    {
+        int rolledNumber = random.Next(0, 11); 
+        counts[rolledNumber]++;
+    }
+    
+    Console.WriteLine("Number occurrences after 10,000 rolls:");
+    for (int i = 0; i < counts.Length; i++)
+    {
+        Console.WriteLine($"Number {i}: {counts[i]} times");
+    }
+}
+RandomCounts();
